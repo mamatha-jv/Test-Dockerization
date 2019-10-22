fec4592ba9e7b7fa0a74074b4c8af5b3edae59ad
Mamatha J V <mamatha.jv@in.ibm.com>
2019-10-22 21:14:55 +0200

    Update perl-test.sh

--- perl-test/perl-test.sh
+++ perl-test/perl-test.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 # Licensed under the Apache License, Version 2.0 (the "License");
 
-# For this example, we will simply check syntax of hello.pl and run it
+# For this example, we will simply check syntax of Test2.pl and run it
 cd /perl-testcase
 
 pwd
 
 set -e
-echo "Compile and execute Test1" && \
-perl -c hello.pl && \
-perl hello.pl
+echo "Checking syntax of Test2.pl and executing it" && \
+perl -c Test2.pl && \
+perl Test2.pl
 set +e
